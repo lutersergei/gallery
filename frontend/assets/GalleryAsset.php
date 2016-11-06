3939a1b8e94ee8d9b12ff504af8ff1fac5ec1a8c
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-06 16:59:08 +0700

    add layout and asset

---
+++ frontend/assets/GalleryAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Gallery asset bundle.
+ */
+class GalleryAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/style.css',
+        'https://fonts.googleapis.com/css?family=Comfortaa:400,300&subset=latin,cyrillic'
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
+    ];
+}
---
+++ frontend/views/layouts/gallery.php
@@ -0,0 +1,56 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\GalleryAsset;
+use common\widgets\Alert;
+use yii\helpers\Url;
+
+GalleryAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!doctype html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+            <div class="row ">
+                <ul class="nav nav-tabs">
+                    <li><a href="<?= Url::to(['/site/index']) ?>">Gallery</a></li>
+                    <li><a href="<?= Url::to(['/site/add']) ?>">Add Image </a></li>
+                    <?php
+                    if (Yii::$app->user->isGuest):
+                        ?>
+                        <li><a href="<?= Url::to(['/user/signup']) ?>">Sign Up</a></li>
+                        <li><a href="<?= Url::to(['/user/login']) ?>">Login</a></li>
+                        <?php
+                    else:
+                        ?>
+                        <li role="presentation" ><a href=".<?= Url::to(['/user/profile']) ?>">Settings</a></li>
+                        <?php
+                    endif;
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <?= $content ?>
+</div>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
