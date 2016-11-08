0054997c3bb3535b78829927660e7b6a7de58b22
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-08 13:35:26 +0700

    add view Add

---
+++ frontend/views/site/upload.php
@@ -0,0 +1,24 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $model \common\models\Image */
+/* @var $categories \common\models\Category[] */
+
+use yii\helpers\ArrayHelper;
+use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Html;
+$this->title = 'Загрузка в галерею';
+?>
+<h1>Загрузка файлов в галерею</h1>
+<p class="bg-danger">Максимальный размер файла 5Mb !</p>
+<?php
+$form = ActiveForm::begin([
+    'id' => 'ImageUploadForm',
+    'layout' => 'default',
+]);
+echo $form->field($model, 'imageFile', [
+    'enableLabel' => false
+])->fileInput();
+echo $form->field($model, 'category_id')->dropDownList(ArrayHelper::map($categories, 'id', 'category'));
+echo $form->field($model, 'description')->textarea(['autofocus' => true, 'rows' => 5]);
+echo Html::submitButton(Html::icon('glyphicon btn-glyphicon glyphicon-ok img-circle text-success') . 'Add', ['class' => 'btn icon-btn btn-success']);
+ActiveForm::end();
