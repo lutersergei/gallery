9cba9cda134c98a94b93050cff2a06b0182efac1
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-10 15:11:18 +0700

    style fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,9 +13,11 @@ $this->title = 'Галерея';
 ?>
 <div class="row">
     <div class="col-sm-9">
+        <h1 class="title">Image Gallery</h1>
+    </div>
+    <div class="col-sm-9">
         <?= Alert::widget() ?>
         <div class="row">
-            <h1 class="title">Image Gallery</h1>
             <?php foreach ($images as $image):?>
                 <div class="thumbnail_new">
                     <a class="thumbnail" href="<?= Url::to(['site/view', 'id' => $image->id]) ?>">
