7a5d2bf7cb506bc9f908c069960c8edcff594efc
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-10 11:30:44 +0700

    fix layout aside

--- frontend/views/layouts/gallery.php
+++ frontend/views/layouts/gallery.php
@@ -58,61 +58,7 @@ GalleryAsset::register($this);
         ]);
         ?>
     </div>
-    <div class="row">
-        <div class="col-sm-9">
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
-        <aside class="col-sm-3">
-            <h3>Recent Posts</h3>
-            <div class="list-group">
-                <a href="#" class="list-group-item active">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, optio eum …
-                    </p>
-                </a>
-                <a href="#" class="list-group-item">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, optio eum …
-                    </p>
-                </a>
-                <a href="#" class="list-group-item">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, optio eum …
-                    </p>
-                </a>
-                <a href="#" class="list-group-item">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, optio eum …
-                    </p>
-                </a>
-            </div>
-
-
-
-
-
-
-            <div class="container">
-                <h3>Follow Us</h3>
-                <div class="social">
-                    <a href=""><i class="fa fa-3x fa-facebook-square"></i></a>
-                    <a href=""><i class="fa fa-3x fa-twitter-square"></i></a>
-                    <a href=""><i class="fa fa-3x fa-linkedin-square"></i></a>
-                    <a href=""><i class="fa fa-3x fa-google-plus-square"></i></a>
-                </div>
-
-            </div>
-
-
-
-
-        </aside>
-    </div>
+    <?= $content ?>
 </div>
 <?php $this->endBody() ?>
 </body>
