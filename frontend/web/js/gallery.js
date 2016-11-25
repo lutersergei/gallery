03b6058ac5348416423f4c187d29b51b69cb7c6d
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-25 15:13:36 +0700

    add mediaQuery listener

--- frontend/web/js/gallery.js
+++ frontend/web/js/gallery.js
@@ -1,17 +1,44 @@
 $(document).ready(function() {
+
     var gallery = $('#gallery');
     if (gallery.length) {
-        gallery.justifiedGallery({
-            border: 1,
-            justifyThreshold: 0.75,
-            margins: 10,
-            rowHeight: 130,
-            maxRowHeight: 150
-        }).on('jg.complete', function() {
-            gallery.lightGallery({
-                selector: '.picture',
-                thumbnail: true
-            });
-        });
+
+        var handleMatchMedia = function (mediaQuery) {
+                if (mediaQuery.matches) {
+                    console.log('<');
+                    gallery.justifiedGallery({
+                        border: 1,
+                        justifyThreshold: 0.75,
+                        margins: 10,
+                        rowHeight: 80,
+                        maxRowHeight: 100
+                    }).on('jg.complete', function () {
+                        gallery.lightGallery({
+                            selector: '.picture',
+                            thumbnail: true
+                        });
+                    });
+                } else {
+                    console.log('>');
+                    gallery.justifiedGallery({
+                        border: 1,
+                        justifyThreshold: 0.75,
+                        margins: 10,
+                        rowHeight: 130,
+                        maxRowHeight: 150
+                    }).on('jg.complete', function () {
+                        gallery.lightGallery({
+                            selector: '.picture',
+                            thumbnail: true
+                        });
+                    });
+                }
+            },
+
+            mql = window.matchMedia('all and (max-width: 480px)');
+
+        handleMatchMedia(mql);
+        mql.addListener(handleMatchMedia);
+
     }
 });
