4c2eba655d0835c5b944fb63bef42ce4e44d201e
Lyuter Sergei <luter.sergei@ya.ru>
2016-11-30 22:01:01 +0700

    fix

--- frontend/web/js/gallery.js
+++ frontend/web/js/gallery.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
 
+
     var gallery = $('#gallery');
     if (gallery.length) {
 
         var handleMatchMedia = function (mediaQuery) {
                 if (mediaQuery.matches) {
-                    console.log('<');
                     gallery.justifiedGallery({
-                        border: 1,
+                        border: 5,
                         justifyThreshold: 0.75,
-                        margins: 10,
+                        margins: 15,
                         rowHeight: 80,
                         maxRowHeight: 100
                     }).on('jg.complete', function () {
                         gallery.lightGallery({
-                            selector: '.picture',
+                            selector: '.picture a',
                             thumbnail: true
                         });
                     });
                 } else {
-                    console.log('>');
                     gallery.justifiedGallery({
-                        border: 1,
+                        border: 5,
                         justifyThreshold: 0.75,
-                        margins: 10,
+                        margins: 15,
                         rowHeight: 130,
                         maxRowHeight: 150
                     }).on('jg.complete', function () {
                         gallery.lightGallery({
-                            selector: '.picture',
+                            selector: '.picture a',
                             thumbnail: true
                         });
                     });
